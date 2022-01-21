fa2ad44b99598275eb2c939378f4e0c4e18c7e01
luizsaulo <luizsaulo@yahoo.com.br>
2022-01-20 21:09:19 -0300

    finalização da função saque

--- index.js
+++ index.js
@@ -217,11 +217,41 @@ function withdraw() {
 
             const amount = answer['amount']
 
-            console.log(amount)
-
-        }).catch(err => console.log(err))
+            removeAmount(accountName, amount)
+            
+        }).catch((err) => console.log(err))
 
     })
-    .catch.err(err=> console.log(err))
+    .catch((err) => console.log(err))
 
+}
+
+function removeAmount(accountName, amount) {
+
+    const accountData = getAccount(accountName)
+
+    if(!amount) {
+        console.log(
+            chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde.')
+        )
+        return withdraw()
+    }
+
+    if(accountData.balance < amount) {
+        console.log(chalk.bgRed.black('Valor indisponível.'))
+        return withdraw()
+    }
+
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function(err) {
+            console.log(err)
+        },
+    )
+
+        console.log(chalk.green(`Foi realizado um saque de R$${amount} da sua conta!`))
+        operation()
 }
